92267b676e5f32ba0a56f2090ab183e58744d7df
thewarsawpakt <88908312+thewarsawpakt@users.noreply.github.com>
2023-07-16 11:50:36 -0500

    Fix typo in generate_keys.py

--- generate_keys.py
+++ generate_keys.py
@@ -3,5 +3,5 @@ import os
 os.mkdir("./payloads")
 
 for to_generate in ["ack", "syn"]:
-    with open(f"./paylods/{to_generate}", "wb") as f:
+    with open(f"./payloads/{to_generate}", "wb") as f:
         f.write(os.urandom(2048))
